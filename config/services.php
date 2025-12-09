17e1dea11705c2f1754266c62dff7b39ab0a434c
stovburtm-web <stovburtm@gmail.com>
2025-12-09 19:58:45 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -2,43 +2,35 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'postmark' => [
-        'key' => env('POSTMARK_API_KEY'),
+    'mailgun' => [
+        'domain'   => env('MAILGUN_DOMAIN'),
+        'secret'   => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme'   => 'https',
     ],
 
-    'resend' => [
-        'key' => env('RESEND_API_KEY'),
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
+        'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
+    // 👇 Ми вже додавали це раніше
     'horoshop' => [
-        'domain'   => env('contractor.kiev.ua'),
-        'login'    => env('owner'),
-        'password' => env('welcome25201'),
+        'domain'   => env('HOROSHOP_DOMAIN'),
+        'login'    => env('HOROSHOP_API_LOGIN'),
+        'password' => env('HOROSHOP_API_PASSWORD'),
+    ],
+
+    // 👇 Додаємо налаштування OpenAI
+    'openai' => [
+        'api_key' => env('OPENAI_API_KEY'),
+        'model'   => env('OPENAI_MODEL', 'gpt-4.1-mini'),
+        'base_url'=> env('OPENAI_BASE_URL', 'https://api.openai.com/v1'),
     ],
 
 ];
-

5088f4e136cd67152ce3d929cbc4a32afedbe965
stovburtm-web <stovburtm@gmail.com>
2025-12-09 20:57:42 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',      // 👈 ДОДАЛИ ОЦЕ
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();

92d461bd97d806770d1fd5e313f8348b001f1145
stovburtm-web <stovburtm@gmail.com>
2025-12-10 13:01:34 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ChatController;
 use App\Http\Controllers\Api\OrderStatusController;
+use App\Http\Controllers\Api\DebugProductsController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,5 @@ Route::post('/chat', [ChatController::class, 'handle']);
 
 // Статус замовлення по order_id
 Route::post('/order-status', [OrderStatusController::class, 'show']);
+
+Route::get('/debug/products', [DebugProductsController::class, 'index']);

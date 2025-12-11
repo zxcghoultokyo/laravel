d96c8840c75c4f2c6fb82f9b939cb764d1e69f11
stovburtm-web <stovburtm@gmail.com>
2025-12-11 13:20:45 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-    $this->call(ScenarioSeeder::class);
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Тут викликаємо всі сідери, які нам потрібні
+        $this->call([
+            ScenarioSeeder::class,
         ]);
     }
 }

7d60167b549ab72c62d0b71c5360a73e88955295
stovburtm-web <stovburtm@gmail.com>
2025-12-12 14:12:53 +0200

    Create 2025_12_12_121235_create_categories_table.php

---
+++ database/migrations/2025_12_12_121235_create_categories_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('path')->unique();
+            $table->string('path_norm')->index();
+            $table->string('slug')->unique();
+            $table->unsignedInteger('products_count')->default(0);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};

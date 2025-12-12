42a26f1b0285f03d8073adc64f238f37f3d37be8
stovburtm-web <stovburtm@gmail.com>
2025-12-12 17:55:01 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\SyncHoroshopProducts::class,
         \App\Console\Commands\BuildProductAiIndex::class,
         \App\Console\Commands\SearchEvaluate::class,
+        \App\Console\Commands\SearchSeedEval::class,
     ];
 
     protected function schedule(Schedule $schedule): void
